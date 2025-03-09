75f39ce68f5e87e7eb22a4319175eb6896a74c48
dys1128 <78744049+dys1128@users.noreply.github.com>
2025-03-09 19:52:35 +0900

    d

---
+++ js/code-viewer.js
@@ -0,0 +1,12 @@
+// code-viewer.js
+document.addEventListener('DOMContentLoaded', function () {
+  fetch(location.href)
+    .then((response) => response.text())
+    .then((text) => {
+      // 코드 블록 추가
+      let pre = document.createElement('pre');
+      pre.textContent = text;
+      document.body.appendChild(document.createElement('hr'));
+      document.body.appendChild(pre);
+    });
+});
